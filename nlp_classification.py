31985080edc97b44e56194ad29bdd70bc97c618f
dstarkey23 <david.starkey@vortexa.com>
2020-07-27 12:12:08 +0100

    add NLP update typos in jupyter notebook

--- nlp_classification.py
+++ nlp_classification.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import metrics
 import numpy as np
 #load train data
 df = pd.read_csv('./nlp_data/train.csv')
